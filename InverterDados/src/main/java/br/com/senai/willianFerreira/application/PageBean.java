af94d5172280951341adc74809e89698257c754d
Willian Ferreira <90736217+zReBolTz@users.noreply.github.com>
2022-04-26 15:49:24 -0300

    Add files via upload

---
+++ InverterDados/src/main/java/br/com/senai/willianFerreira/application/PageBean.java
@@ -0,0 +1,45 @@
+package br.com.senai.willianFerreira.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jboss.crypto.digest.SHAReverseInterleave;
+
+@Named
+@RequestScoped
+@SuppressWarnings("serial")
+
+public class PageBean implements Serializable  {
+	
+ private String texto;
+ private String textoInvertido;
+public String getTexto() {
+	return texto;
+}
+public void setTexto(String texto) {
+	this.texto = texto;
+}
+public String getTextoInvertido() {
+	return textoInvertido;
+}
+public void setTextoInvertido(String textoInvertido) {
+	this.textoInvertido = textoInvertido;
+}
+ 
+ 
+public String inversao() {
+	
+	textoInvertido= new StringBuilder(texto).reverse().toString();
+	return textoInvertido;
+	
+}
+
+
+
+	
+
+}
