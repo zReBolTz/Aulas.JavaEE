b03039649f990b8b74d92fac30d783c19e1c4104
Willian Ferreira <90736217+zReBolTz@users.noreply.github.com>
2022-06-11 14:39:36 -0300

    Update PageBean.java

--- ProjetoNavegacaoTelas/src/main/java/br/com/senai/willianFerreira/application/PageBean.java
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/willianFerreira/application/PageBean.java
@@ -7,11 +7,14 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
+//RequestScoped= determina o tempo de vida que terá está classe
+//Está classe irá funcionar apenas quando for feita uma requisição
 @RequestScoped
 @SuppressWarnings("serial")
 public class PageBean implements Serializable {
 	
-	
+	//Criando um enum com as páginas
+	//Enum= enum é uma maneira de dizer que sempreterá um conjunto de informações que podem se repetir durante o site
 	public enum Pages{
 		page_a, page_b
 	}
@@ -25,7 +28,7 @@ public class PageBean implements Serializable {
 	public void setPage(String page) {
 		this.page = page;
 	}
-	
+	//Comparando se uma String/Objeto é igual o valor que está determinada na váriavel"page"
 	public Pages go_to(){
 		if("a".equals(page)) {
 			return Pages.page_a;
